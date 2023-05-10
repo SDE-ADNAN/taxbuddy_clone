c9584a80e8c351b3533dcab9811edeb103403ae0
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-05-10 12:10:51 +0530

    did hero

---
+++ src/components/Pages/HeroSec/HeroSec.jsx
@@ -0,0 +1,20 @@
+import "./HeroSec.scss";
+import Lines from "../../../medias/lines.svg"
+
+
+
+
+const HeroSec = (props)=>{
+    return(
+        <div className="hero__main">
+            <div className="lines_img">
+                <img src={Lines}></img>
+            </div>
+            <div className="elipse"></div>
+            dfs
+        </div>
+    )
+}
+
+
+export default HeroSec;
