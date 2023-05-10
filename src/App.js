fd84c4563cc57b57a84a95a77d19cdf31a92f27a
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-05-10 18:20:34 +0530

    styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import HeroSec from "./components/Pages/HeroSec/HeroSec";
 import "./styles.scss";
 import GoogleFontLoader from "react-google-font-loader";
 import logos from "./medias/brand-logos.svg"
+import StatsSec from "./components/Pages/StatsSec/StatsSec";
 
 export default function App() {
 
+const BrandLogos =()=> <div className="brand_logo">
+<img src={logos}></img>
+<div className="matrix_left"></div>
+<div className="matrix_right"></div>
+</div>
 
   return (
     <div className="main">
@@ -22,11 +28,8 @@ export default function App() {
         subsets={["cyrillic-ext", "greek"]}
       />
       <HeroSec/>
-      <div className="brand_logo">
-        <img src={logos}></img>
-        <div className="matrix_left"></div>
-        <div className="matrix_right"></div>
-      </div>
+      <BrandLogos/>
+      <StatsSec/>
     </div>
   );
 }
---
+++ src/components/Pages/StatsSec/StatsSec.jsx
@@ -0,0 +1,9 @@
+import "./StatsSec.scss"
+
+const StatsSec = (props)=>{
+    return(
+        <div></div>
+    )
+}
+
+export default StatsSec;
