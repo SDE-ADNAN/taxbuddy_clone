7a95d8ba64b09fdbb63feceb362a25518b8af497
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-05-10 12:44:45 +0530

    nav styles

--- src/components/Pages/HeroSec/HeroSec.jsx
+++ src/components/Pages/HeroSec/HeroSec.jsx
@@ -1,17 +1,23 @@
 import "./HeroSec.scss";
 import Lines from "../../../medias/lines.svg"
+import Logo from "../../../medias/logo.svg"
 
 
 
 
-const HeroSec = (props)=>{
-    return(
+const HeroSec = (props) => {
+    return (
         <div className="hero__main">
             <div className="lines_img">
                 <img src={Lines}></img>
             </div>
             <div className="elipse"></div>
-            dfs
+            <div className="content_main_div">
+                <div className="nav__main">
+                    <img src={Logo}></img>
+                </div>
+            </div>
+
         </div>
     )
 }
